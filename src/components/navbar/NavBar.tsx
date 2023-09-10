c6d16e974a6fc479896f141f12d1e77906f3fb80
VitoDarell <vitodarell@gmail.com>
2023-09-09 18:56:17 -0700

    just minor changes like hover stuff on navbar
    also improved a tiny bit of the draft of homepage

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -21,10 +21,10 @@ export const NavBar = () => {
           className={`${styles.navBarText} absolute left-0 right-0 mx-auto flex justify-center gap-6`}
           style={{ width: "fit-content" }}
         >
-          <Link to="/about">About</Link>
-          <Link to="/games">Games</Link>
-          <Link to="/events">Events</Link>
-          <Link to="/partners">Partners</Link>
+          <Link className="hover:opacity-70 transition" to="/about">About</Link>
+          <Link className="hover:opacity-70 transition" to="/games">Games</Link>
+          <Link className="hover:opacity-70 transition" to="/events">Events</Link>
+          <Link className="hover:opacity-70 transition" to="/partners">Partners</Link>
         </div>
 
         <Button className="bg-transparent text-white border-2 py-2 px-6 absolute right-6 text font-normal">Join our Discord Server</Button>
--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,9 +1,15 @@
+import Button from "../../components/Button/Button";
+
 export const HomePage = () => {
   return (
-    <main>
-      <h1 className="text-3xl font-extrabold mb-4">Grow Games With Us From The Ground Up</h1>
+    <main className="flex flex-col justify-center align-middle">
+      <h1 className="text-5xl font-extrabold mb-4">Grow Games With Us From The Ground Up</h1>
 
-      <p>We bring together artists</p>
+      <p>We bring together artists, programmers, writers, and musicians to make games! No previous experience required, you can join a team and start working on a game with other budding developers! 
+        Keep an eye on our discord for the sign up forms, which will be going up after our Launch Party!
+      </p>
+
+      <Button className="bg-green m-auto">Join our Discord Server</Button>
     </main>
   );
 };
