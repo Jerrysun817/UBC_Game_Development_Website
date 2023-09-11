0a70455c7667f8afe7eb43dd67227b89845c0303
Erick Jovan Muljadi <ejovan2004@gmail.com>
2023-09-10 22:40:30 -0700

    add padding

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,3 +1,3 @@
 export const HomePage = () => {
-  return <div className=" bg-blue-500 text-white p-4 h-[1000px]">HomePage</div>;
+  return <div className=" bg-blue-500 text-white p-4 h-[1000px] pt-[150px]">HomePage</div>;
 };
