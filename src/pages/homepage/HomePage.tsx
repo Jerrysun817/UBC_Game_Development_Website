0b6b3d32fce7b6a2540fb554780886a175151d32
VitoDarell <vitodarell@gmail.com>
2023-09-18 17:11:27 -0700

    landing page responsiveness
    landing page should now be responsive (I think lol).
    also added sponsors section with placeholder logos, responsiveness is also added, with some further modification planned.

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,11 +1,14 @@
-import Button from "../../components/Button/Button";
+import { SiAdobeillustrator } from "react-icons/si";
+
+import Button from "../../components/Button/Button";  
 
 export const HomePage = () => {
   return (
-    <main className="">
-      <section className="h-screen flex flex-col items-center">
+    <main className="w-full h-full overflow-hidden relative flex flex-col">
+      <section className="h-5/6 flex flex-col items-center gap-6">
         <h1 className="
-        lg:text-7xl
+        w-full
+        h-full
         md:text-6xl
         xl:w-2/3
         text-4xl
@@ -13,26 +16,39 @@ export const HomePage = () => {
         mb-4
         text-center 
         p-5
-        mt-20
+        md:mt-20
         ">
           Grow Games With Us From The Ground Up
         </h1>
 
-        <p className="text-center w-2/3 p-5">
+        <p className="text-center w-2/3 p-5 md:w-1/2">
           We bring together artists, programmers, writers, and musicians to make
           games! No previous experience required, you can join a team and start
           working on a game with other budding developers!
         </p>
 
-        <p className="mt-5 w-2/3 text-center">
+        <p className="mt-5 w-2/3 text-center md:w-1/2">
           Keep an eye on our discord for the sign up forms, which will be going up
           after our Launch Party!
         </p>
 
-        <Button className="bg-green m-auto my-10">Join our Discord Server</Button>
+        <Button className="bg-green m-auto mt-10 mb-40">Join our Discord Server</Button>
       </section>
-      <section >
-        <div className="bg-white text-black">Test</div>
+      <section className="flex flex-col items-center w-full bg-white h-auto py-10 gap-8">
+        <h2 className="text-black font-bold text-2xl">With The Support of:</h2>
+
+        <div className="flex flex-col md:flex-row gap-10">
+          <SiAdobeillustrator className="text-black text-5xl"/>
+          <SiAdobeillustrator className="text-black text-5xl"/>
+          <SiAdobeillustrator className="text-black text-5xl"/>
+          <SiAdobeillustrator className="text-black text-5xl"/>
+          <SiAdobeillustrator className="text-black text-5xl"/>
+        </div>
+        
+      </section>
+
+      <section>
+
       </section>
     </main>
   );
